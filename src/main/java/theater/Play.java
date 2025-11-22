5235cbcb4af237c02ad91e1f5697e82e1455ac2d
AndyYanxunJiang <ayx.jiang@mail.utoronto.ca>
2025-11-21 23:38:43 -0500

    Task 1.1: Fix CheckStyle issues

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,25 @@
 package theater;
 
+/**
+ * Play.
+ *
+ * @null
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
